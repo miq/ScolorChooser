3a433429a353227ffe69fac649047c76e0d4953b
Mihael Vrbanec <miqster@gmx.net>
2009-12-20 22:36:20 +0100

    -reduce visibility of members

--- src/de/miq/scolor/ScalaColorChooserPanel.scala
+++ src/de/miq/scolor/ScalaColorChooserPanel.scala
@@ -11,15 +11,15 @@ import de.miq.scolor.gradient._
 
 class ScalaColorChooserPanel extends AbstractColorChooserPanel {
 
-  val colorControls = List(
+  private val colorControls = List(
 		  new ColorComponentControl("H", new HueGradientCalculator(), 360, colorChangedCallback),
 		  new ColorComponentControl("S", new SaturationGradientCalculator(), 100, colorChangedCallback),
 		  new ColorComponentControl("B", new BrightnessGradientCalculator(), 100, colorChangedCallback),
 		  new ColorComponentControl("R", new RedGradientCalculator(), 255, colorChangedCallback),
 		  new ColorComponentControl("G", new GreenGradientCalculator(), 255, colorChangedCallback),  
 		  new ColorComponentControl("B", new BlueGradientCalculator(), 255, colorChangedCallback))
-  val hexDisplay = new HexColorDisplay()
-  val colorPanel = new JPanel
+  private val hexDisplay = new HexColorDisplay()
+  private val colorPanel = new JPanel
   
   override def getLargeDisplayIcon : Icon = {
     return null
@@ -33,7 +33,7 @@ class ScalaColorChooserPanel extends AbstractColorChooserPanel {
     return "Scala Mighty"
   }
   
-  def colorChangedCallback(newColor: Color) : Unit = {
+  private def colorChangedCallback(newColor: Color) : Unit = {
     colorControls.foreach(cc => cc.color = newColor)
     colorPanel.setBackground(newColor)
     hexDisplay.setColor(newColor)
