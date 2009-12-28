c4892e7638a486327c321246c315e487b843f65a
Mihael Vrbanec <miqster@gmx.net>
2009-12-28 12:11:13 +0100

    -whitespace fixes

--- src/de/miq/scolor/HexColorDisplay.scala
+++ src/de/miq/scolor/HexColorDisplay.scala
@@ -10,11 +10,11 @@ class HexColorDisplay extends JPanel(new MigLayout("insets 0")) {
   val hexField = new JTextField()
   hexField.setEditable(false)
   add(hexField, "width 100%")
-  
+
   def setColor(c: Color) = {
     hexField.setText(leadingZeroHex(c.getRed) + leadingZeroHex(c.getGreen) + leadingZeroHex(c.getBlue))
   }
-  
+
   private def leadingZeroHex(i: Int) : String = {
     if (i < 16) {
       return "0" + i.toHexString
