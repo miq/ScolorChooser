f37caf081d96162434e852e4991e56af2253e694
Mihael Vrbanec <miqster@gmx.net>
2009-12-28 12:11:55 +0100

    -use 359 for maximum hue value to avoid strange behaviour falling back to 0 when at 360

--- src/de/miq/scolor/ScalaColorChooserPanel.scala
+++ src/de/miq/scolor/ScalaColorChooserPanel.scala
@@ -12,33 +12,33 @@ import de.miq.scolor.gradient._
 class ScalaColorChooserPanel extends AbstractColorChooserPanel {
 
   private val colorControls = List(
-		  new ColorComponentControl("H", new HueGradientCalculator(), 360, colorChangedCallback),
+		  new ColorComponentControl("H", new HueGradientCalculator(), 359, colorChangedCallback),
 		  new ColorComponentControl("S", new SaturationGradientCalculator(), 100, colorChangedCallback),
 		  new ColorComponentControl("B", new BrightnessGradientCalculator(), 100, colorChangedCallback),
 		  new ColorComponentControl("R", new RedGradientCalculator(), 255, colorChangedCallback),
-		  new ColorComponentControl("G", new GreenGradientCalculator(), 255, colorChangedCallback),  
+		  new ColorComponentControl("G", new GreenGradientCalculator(), 255, colorChangedCallback),
 		  new ColorComponentControl("B", new BlueGradientCalculator(), 255, colorChangedCallback))
   private val hexDisplay = new HexColorDisplay()
   private val colorPanel = new JPanel
-  
+
   override def getLargeDisplayIcon : Icon = {
     return null
   }
-  
+
   override def getSmallDisplayIcon : Icon = {
     return null
   }
-  
+
   override def getDisplayName : String = {
     return "Scala Mighty"
   }
-  
+
   private def colorChangedCallback(newColor: Color) : Unit = {
     colorControls.foreach(cc => cc.color = newColor)
     colorPanel.setBackground(newColor)
     hexDisplay.setColor(newColor)
   }
-  
+
   override def buildChooser = {
     setLayout(new MigLayout())
     colorPanel.setMinimumSize(100, 100)
@@ -51,6 +51,6 @@ class ScalaColorChooserPanel extends AbstractColorChooserPanel {
     add(hexDisplay, "spanx, growx")
     colorChangedCallback(Color.GREEN)
   }
-  
+
   override def updateChooser = {}
 }
