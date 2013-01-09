4018d82a91f5ce312f13ac70b24afd727aa1f2e9
Mihael Koep <miqster@gmx.de>
2013-01-09 21:47:32 +0100

    Improve test readability by using should matchers

--- src/test/scala/de/miq/scolor/gradient/HueGradientCalculatorTest.scala
+++ src/test/scala/de/miq/scolor/gradient/HueGradientCalculatorTest.scala
@@ -2,23 +2,24 @@ package de.miq.scolor.gradient
 
 import org.scalatest.FunSpec
 import java.awt.Color
+import org.scalatest.matchers.ShouldMatchers
 
-class HueGradientCalculatorTest extends FunSpec {
+class HueGradientCalculatorTest extends FunSpec with ShouldMatchers {
 
   describe("Hue component of hue gradient calculator") {
     it("should be 0 for red") {
-      assert(0 === new HueGradientCalculator().computeComponent(Color.RED))
+      new HueGradientCalculator().computeComponent(Color.RED) should equal (0)
     }
     it("should be 120 for green") {
-      assert(120 === new HueGradientCalculator().computeComponent(Color.GREEN))
+      new HueGradientCalculator().computeComponent(Color.GREEN) should equal (120)
     }
     it("should be 240 for blue") {
-      assert(240 === new HueGradientCalculator().computeComponent(Color.BLUE))
+      new HueGradientCalculator().computeComponent(Color.BLUE) should equal (240)
     }
     it("should be 0 for gray, black and white") {
-      assert(0 === new HueGradientCalculator().computeComponent(Color.GRAY))
-      assert(0 === new HueGradientCalculator().computeComponent(Color.BLACK))
-      assert(0 === new HueGradientCalculator().computeComponent(Color.WHITE))
+      new HueGradientCalculator().computeComponent(Color.GRAY) should equal (0)
+      new HueGradientCalculator().computeComponent(Color.BLACK) should equal (0)
+      new HueGradientCalculator().computeComponent(Color.WHITE) should equal (0)
     }
   }
 }
